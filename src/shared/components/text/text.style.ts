8cb78f4b7ff9c811dd4033a1fc171927b5b50135
Airton Oliveira <airton.oliveira@fabmed.corp>
2023-12-29 10:43:16 -0300

    Inserido os tipos de texto em Text

--- src/shared/components/text/text.style.ts
+++ src/shared/components/text/text.style.ts
@@ -3,11 +3,13 @@ import styled from "styled-components/native";
 interface ContainerTextProps {
     color?: string;
     fontSize: string;
+    fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
 
 };
 
 export const ContainerText = styled.Text<ContainerTextProps>`
     ${(props: { color: any; }) => (props.color ? `color: ${props.color}` : '')};
 
+    font-family: ${(props) => props.fontFamily};
     font-size: ${(props: { fontSize: any; }) => props.fontSize};
 `;
