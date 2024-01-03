d9d04bd3dd0a133a3a5aa0d657f29ab2b1c4e637
Airton Oliveira <airton.oliveira@fabmed.corp>
2024-01-03 09:54:08 -0300

    Inserido icones no projeto

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -4,6 +4,7 @@ import Button from "../../../shared/components/button/Button";
 import { ContainerLogin } from "../styles/login.style";
 import Input from "../../../shared/components/input/Input";
 import { theme } from "../../../shared/themes/theme";
+import { Icon } from "../../../shared/components/icon/icon";
 
 
 
@@ -14,6 +15,7 @@ const Login = () => {
     return (
         <View>
             <ContainerLogin>
+                <Icon name="home3"/>
                 <Input placeholder="Digite seu email" title="Email:"/>
                 <Button type={theme.buttons.buttonsTheme.primary} 
                 margin="16px"
